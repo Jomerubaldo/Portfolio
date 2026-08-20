f2f1fb1ceb1f9cb28dee1ba25cff24c29e15bc3d
Your Name <jomerubaldo@gmail.com>
2026-08-21 01:39:22 +0800

    added projects layout

---
+++ frontend/function.js
@@ -0,0 +1,78 @@
+function bhmsOpenPhoto() {
+  const bhmsModal = document.getElementById('bhmsPhotoModal');
+  bhmsModal.classList.remove('hidden');
+  bhmsModal.classList.add('flex');
+}
+
+function bhmsClosePhoto() {
+  const bhmsModal = document.getElementById('bhmsPhotoModal');
+  bhmsModal.classList.add('hidden');
+  bhmsModal.classList.remove('flex');
+}
+
+function agssomsOpenPhoto() {
+  const agssomsModal = document.getElementById('agssomsPhotoModal');
+  agssomsModal.classList.remove('hidden');
+  agssomsModal.classList.add('flex');
+}
+
+function agssomsClosePhoto() {
+  const agssomsModal = document.getElementById('agssomsPhotoModal');
+  agssomsModal.classList.add('hidden');
+  agssomsModal.classList.remove('flex');
+}
+
+function rwsOpenPhoto() {
+  const agssomsModal = document.getElementById('rwsPhotoModal');
+  agssomsModal.classList.remove('hidden');
+  agssomsModal.classList.add('flex');
+}
+
+function rwsClosePhoto() {
+  const agssomsModal = document.getElementById('rwsPhotoModal');
+  agssomsModal.classList.add('hidden');
+  agssomsModal.classList.remove('flex');
+}
+
+function tdwOpenPhoto() {
+  const agssomsModal = document.getElementById('tdwPhotoModal');
+  agssomsModal.classList.remove('hidden');
+  agssomsModal.classList.add('flex');
+}
+
+function tdwClosePhoto() {
+  const agssomsModal = document.getElementById('tdwPhotoModal');
+  agssomsModal.classList.add('hidden');
+  agssomsModal.classList.remove('flex');
+}
+
+const slider = document.querySelector('#scroll-container');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('cursor-grabbing');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('cursor-grabbing');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('cursor-grabbing');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
