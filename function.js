d5b39810768c56168efd98de42434944f359a32a
Your Name <jomerubaldo@gmail.com>
2026-09-07 00:15:31 +0800

    feat: dark mode

--- function.js
+++ function.js
@@ -1,3 +1,40 @@
+// theme toggle darkmode
+
+const buttonToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+
+const lightIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>`;
+
+const darkIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>`;
+
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+}
+
+function updateIcon() {
+  themeIcon.innerHTML = document.documentElement.classList.contains('dark')
+    ? darkIcon
+    : lightIcon;
+}
+
+updateIcon();
+
+buttonToggle.addEventListener('click', () => {
+  const isDark = document.documentElement.classList.toggle('dark');
+  localStorage.theme = isDark ? 'dark' : 'light';
+
+  themeIcon.classList.add('opacity-0', 'rotate-90', 'scale-0');
+
+  setTimeout(() => {
+    updateIcon();
+    themeIcon.classList.remove('opacity-0', 'rotate-90', 'scale-0');
+  }, 100);
+});
+
 // bhms modal
 function bhmsOpenPhoto() {
   const bhmsModal = document.getElementById('bhmsPhotoModal');
@@ -89,7 +126,7 @@ function closeCert3() {
   modal3.classList.remove('flex');
 }
 
-// 
+//
 const slider = document.querySelector('#scroll-container');
 
 let isDown = false;
