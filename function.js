1db971b3f3b692d65dae3e103a338854de193c20
Your Name <jomerubaldo@gmail.com>
2026-09-07 00:22:12 +0800

    add: comments

--- function.js
+++ function.js
@@ -1,12 +1,15 @@
 // theme toggle darkmode
 
+// dom
 const buttonToggle = document.getElementById('theme-toggle');
 const themeIcon = document.getElementById('theme-icon');
 
+// icons
 const lightIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>`;
 
 const darkIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>`;
 
+// check prefer user theme
 if (
   localStorage.theme === 'dark' ||
   (!('theme' in localStorage) &&
@@ -126,7 +129,6 @@ function closeCert3() {
   modal3.classList.remove('flex');
 }
 
-//
 const slider = document.querySelector('#scroll-container');
 
 let isDown = false;
